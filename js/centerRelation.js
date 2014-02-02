118167a306179af5509ed72aab71aef40aa38fd4
Min Huang <444792006@qq.com>
2014-02-02 13:47:34 +0800

    version 1.0

--- js/centerRelation.js
+++ js/centerRelation.js
@@ -18,12 +18,12 @@ var CRcenterY;
   > Arguments
 
     - centerTEXT (string) the text of the center
-    - DIVid (string) the id of a div dom to put the svg in
+    - DIVid (string) the id of a div dom to put the graph in
     - list (array of object) the type of that in list is object with attribute -name(string) and -num(number)
       the larger the list[i].num is, the more center the list[i].name is
-    - pWidth (number) the width of svg
-    - pHeight (number) the height of svg
-    - smallR (number) the smallest distance of svg
+    - pWidth (number) the width of graph
+    - pHeight (number) the height of graph
+    - smallR (number) the smallest radius of circle
     - ANIMATE (string) the type of animate
       it has "linear", "easeIn", "easeOut", "easeInOut", "backIn", "elastic", "bounce"
     - ANIMATETIME (number) the time of the start ANIMATE cost. the unit is “ms”
